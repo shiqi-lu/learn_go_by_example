9306272640b106b7909d18383df81cabec65b7c9
shiqi-lu <traumlou@gmail.com>
2022-01-25 16:56:46 +0800

    ex29

---
+++ 29.timeouts.go
@@ -0,0 +1,36 @@
+package main
+import (
+    "fmt"
+    "time"
+)
+func main() {
+    // 在这个例子中，假如执行一个外部调用， 并在 2 秒后使用通道 c1 返回它的执行结果
+    c1 := make(chan string, 1)
+    go func() {
+        time.Sleep(2 * time.Second)
+        c1 <- "result 1"
+    }()
+
+    // 这里是使用 select 实现一个超时操作
+    // res := <- c1 等待结果，<-time.After 等待超时（1秒钟）以后发送的值
+    // 由于 select 默认处理第一个已准备好的接收操作
+    // 因此如果操作耗时超过了允许的 1 秒的话，将会执行超时 case
+    select {
+    case res := <-c1:
+        fmt.Println(res)
+    case <-time.After(1 * time.Second):
+        fmt.Println("timeout 1")
+    }
+
+    c2 := make(chan string, 1)
+    go func() {
+        time.Sleep(2 * time.Second)
+        c2 <- "result 2"
+    }()
+    select {
+    case res := <-c2:
+        fmt.Println(res)
+    case <-time.After(3 * time.Second):
+        fmt.Println("timeout 2")
+    }
+}
